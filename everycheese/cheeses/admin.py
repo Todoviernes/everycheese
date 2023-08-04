601989ab00c4e6c57f4e9cc701fce7f219a36189
Santiago Vallejo <santiagovallejoc@gmail.com>
2023-08-04 15:38:20 -0600

    Register Cheese model with the admin

--- everycheese/cheeses/admin.py
+++ everycheese/cheeses/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+from everycheese.cheeses.models import Cheese
+
+
+@admin.register(Cheese)
+class CheeseAdmin(admin.ModelAdmin):
+    list_display = ("name", "firmness", "created", "modified")
--- everycheese/cheeses/models.py
+++ everycheese/cheeses/models.py
@@ -18,8 +18,13 @@ class Cheese(TimeStampedModel):
     description = models.TextField("Description", blank=True)
     slug = models.SlugField("Cheese Address",
                             unique=True,
+                            blank=True,
                             )
     firmness = models.CharField("Firmness", max_length=20, choices=Firmness.choices, default=Firmness.UNSPECIFIED)
 
     def __str__(self):
         return self.name
+
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.name)
+        super(Cheese, self).save(*args, **kwargs)
